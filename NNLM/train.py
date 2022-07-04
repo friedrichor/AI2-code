6fb0588c9dc86f9ecf653237fdd09292f259af72
friedrichor <kongfanheng426@163.com>
2022-07-04 20:38:42 +0800

    Update train.py

--- NNLM/train.py
+++ NNLM/train.py
@@ -5,8 +5,7 @@ from torch import nn, optim
 from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
 
-from my_dataset import MyDataSet
-from utils import generate_vocab, generate_dataset, create_lr_scheduler, train_one_epoch
+from utils import generate_vocab, generate_dataset, create_lr_scheduler, train_one_epoch, evaluate
 from model import NNLM
 from params import *
 
@@ -16,6 +15,10 @@ def main():
     print('Using {} dataloader workers every process'.format(nw))
 
     tb_writer = SummaryWriter()
+    # % load_ext tensorboard
+    # % tensorboard --logdir runs
+    if not os.path.exists(models_path):
+        os.makedirs(models_path)
 
     # 生成单词表
     word2index_dict, index2word_dict = generate_vocab(train_path)
@@ -24,6 +27,7 @@ def main():
 
     # 实例化数据集
     train_dataset = generate_dataset(train_path, word2index_dict, n_step)
+    vaild_dataset = generate_dataset(valid_path, word2index_dict, n_step)
 
     train_loader = DataLoader(train_dataset,
                               batch_size=batch_size,
@@ -31,24 +35,41 @@ def main():
                               pin_memory=True,
                               num_workers=nw)
 
+    valid_loader = DataLoader(vaild_dataset,
+                              batch_size=batch_size,
+                              shuffle=False,
+                              pin_memory=True,
+                              num_workers=nw)
+
     model = NNLM(n_class, m, n_step, n_hidden).to(device)
 
-    criterion = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(model.parameters(), lr=0.001)
+    loss_function = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=lr)
     lr_scheduler = create_lr_scheduler(optimizer, len(train_loader), epochs,
                                        warmup=True, warmup_epochs=1)
 
-    # Training
     for epoch in range(epochs):
         # train
         train_loss = train_one_epoch(model=model,
-                                     criterion=criterion,
+                                     loss_function=loss_function,
                                      optimizer=optimizer,
                                      data_loader=train_loader,
                                      device=device,
                                      epoch=epoch,
                                      lr_scheduler=lr_scheduler)
+        # validate
+        valid_loss = evaluate(model=model,
+                              loss_function=loss_function,
+                              data_loader=valid_loader,
+                              device=device,
+                              epoch=epoch)
+
         tb_writer.add_scalar('train_loss', train_loss, epoch)
+        tb_writer.add_scalar('valid_loss', valid_loss, epoch)
+        tb_writer.add_scalar('learning_rate', optimizer.param_groups[0]["lr"], epoch)
+
+        if (epoch + 1) % save_epoch == 0:
+            torch.save(model, os.path.join(models_path, f'weitghts-{epoch + 1}.ckpt'))
 
 
 if __name__ == '__main__':
