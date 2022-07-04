c4b54749068d117a6085aca114130ac03b63d030
friedrichor <kongfanheng426@163.com>
2022-07-04 20:38:40 +0800

    Update params.py

--- NNLM/params.py
+++ NNLM/params.py
@@ -1,21 +1,34 @@
 import os
+import sys
+from pathlib import Path
 import torch
 
+
+FILE = Path(__file__).resolve()
+ROOT = FILE.parents[0]  # root directory
+if str(ROOT) not in sys.path:
+    sys.path.append(str(ROOT))  # add ROOT to PATH
+ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
 
 # 模型相关参数
 n_step = 5  # number of steps, n-1 in paper
 n_hidden = 2  # number of hidden size, h in paper
 m = 2  # embedding size, m in paper
 
-
 # 训练相关参数
 epochs = 200
-batch_size = 16
+batch_size = 256
+lr = 0.001
+
+save_epoch = 50  # 每多少轮保存一次模型
+models_path = ROOT / 'weigths'  # 模型保存路径
+vocab_path = ROOT / 'vocab.json'  # 单词表保存路径
+test_model = ROOT / 'weights' / 'weights-50.ckpt'  # 测试时使用模型
+
 nw = min([os.cpu_count(), batch_size if batch_size > 1 else 0, 8])  # number of workers
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-
 # 数据集路径
 train_path = 'data/train.txt'
-val_path = 'data/valid.txt'
+valid_path = 'data/valid.txt'
 test_path = 'data/test.txt'
